fb7769d3d23cc5db6c231c8514b2a15d3eeb9183
marito <mpicasso98@gmail.com>
2021-05-23 22:44:51 -0300

    phonebook json server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
+import axios from 'axios';
 
 
 
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-      ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState("");
     const [newNumber, setNewNumber] = useState("");
     const [newSearch, setNewSearch] = useState("");
@@ -50,6 +46,16 @@ const App = () => {
     const handleSearchChange=(e)=>{
         setNewSearch(e.target.value);
     }
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response=>{
+                console.log(response.data);
+                setPersons(response.data);
+            })
+
+    },[])
 
     
 
