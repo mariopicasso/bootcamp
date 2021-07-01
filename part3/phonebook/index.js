2edc225920b4307debafeaa4fd9fd7c28ea1aa6b
marito <mpicasso98@gmail.com>
2021-07-01 16:25:55 -0300

    .

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -90,6 +90,6 @@ app.post('/api/persons', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(3001, () => {
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
 })
