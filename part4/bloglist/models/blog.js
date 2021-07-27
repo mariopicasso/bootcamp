7cdbf1070c6f045e5acd842e5d7b2746c49f8264
marito <mpicasso98@gmail.com>
2021-07-27 17:33:44 -0300

    first bloglist stage

---
+++ part4/bloglist/models/blog.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+blogSchema.set('toJSON', {
+  transform: (obj, ret) => {
+    ret.id = obj._id.toString()
+    delete ret._id
+    delete ret.__v
+  }
+})
+module.exports = mongoose.model('Blog', blogSchema)
